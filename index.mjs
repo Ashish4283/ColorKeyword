b3066fd5637cd758fa2733f8599da35b1d9d98cf
Ashish B Jiwa <43324295+Ashish4283@users.noreply.github.com>
2023-11-21 20:53:04 +0530

    Create index.mjs

---
+++ index.mjs
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello JavaScript!</h1>
+`;
+
+import * as Grammarly from "@grammarly/editor-sdk";
+Grammarly.init("client_Uz5fQNEreVi6ZnVq7uHgLE");
